bb2f9a97162aa26455e229fc86a019cd93a2a508
Mankor <igorzhelezno@gmail.com>
2020-07-16 13:39:38 +0200

    possible puppeteer-heroku fix

--- scrapper.js
+++ scrapper.js
@@ -34,7 +34,12 @@ async function getPlayers(page, player_num) {
 }
 
 async function scrape(url) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args : [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ]
+      });
     const page = await browser.newPage();
     await page.goto(url);
 
