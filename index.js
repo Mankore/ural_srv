408413a57c7d789c9b56772091537f2df3dedd39
Mankor <igorzhelezno@gmail.com>
2020-07-16 13:24:27 +0200

    first test commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const { scrape, link } = require('./scrapper');
+
+const app = express();
+
+const SRV_PORT = 3000;
+const request_every = 0.5; // minutes
+
+app.use(helmet());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+let req_date, last_req_date, cached_obj;
+
+app.use('/info', (req, res, next) => {
+    // If it is a first request after restart
+    // Define last request date
+    if (!last_req_date) {
+        last_req_date = new Date();
+        res.locals.approved = true;
+    } else {
+        // Date of a new request
+        req_date = new Date();
+        // If it is a new request under a ${request_every} minutes
+        // Then send a cached object
+        if (req_date - last_req_date < request_every * 60000) {
+            // console.log('send cached obj');
+            res.locals.approved = false;
+        } else {
+            // console.log('send a new request');
+            // Replace a last_req_date with the latest(new) date
+            last_req_date = req_date;
+            res.locals.approved = true;
+        }
+    }
+
+    next();
+})
+
+app.get('/info', (req, res) => {
+    // If Approved, then send a new request
+    // Else send cached object
+    if (res.locals.approved) {
+        scrape(link).then(obj => {
+            cached_obj = obj;
+            res.send(obj);
+        });
+    } else {
+        res.send(cached_obj);
+    }
+});
+
+app.all('*', (req, res) => {
+    res.send('Invalid request');
+});
+
+app.listen(SRV_PORT, () => {
+    console.log(`App is listening on port: ${SRV_PORT}`);
+});
+
---
+++ scrapper.js
@@ -0,0 +1,53 @@
+const puppeteer = require('puppeteer');
+
+const stats_link = 'https://www.gametracker.com/server_info/109.120.135.78:2302/';
+
+function clearString(str) {
+    str = str.replace(/\n/g, '');
+    return str.replace(/\t/g, '');
+}
+
+async function getElemText(page, xPath, prop) {
+    const [el] = await page.$x(xPath);
+    const src = await el.getProperty(prop);
+    return await src.jsonValue();
+}
+
+async function getPlayers(page, player_num) {
+    let players = [];
+
+    if (player_num >= 10) {
+        for (let i = 0; i < 10; i++) {
+            const name = clearString(await getElemText(page, `//*[@id="HTML_online_players"]/div/table/tbody/tr[${i+2}]/td[2]/a`, 'textContent'));
+            const score = clearString(await getElemText(page, `//*[@id="HTML_online_players"]/div/table/tbody/tr[${i+2}]/td[3]`, 'textContent'));
+            players = [...players, {name, score}];
+        }
+    } else {
+        for (let i = 0; i < player_num; i++) {
+            const name = clearString(await getElemText(page, `//*[@id="HTML_online_players"]/div/table/tbody/tr[${i+2}]/td[2]/a`, 'textContent'));
+            const score = clearString(await getElemText(page, `//*[@id="HTML_online_players"]/div/table/tbody/tr[${i+2}]/td[3]`, 'textContent'));
+            players = [...players, {name, score}];
+        }
+    }
+
+    return players;
+}
+
+async function scrape(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const player_num = await getElemText(page, '/html/body/div[2]/div[7]/div[1]/div[3]/span[2]', 'textContent');
+    const player_total = await getElemText(page, '//*[@id="HTML_max_players"]', 'textContent');
+    const map = clearString(await getElemText(page, '//*[@id="HTML_curr_map"]', 'textContent'));
+
+    const players = await getPlayers(page, player_num);
+    const date = new Date();
+
+    browser.close();
+    return {player_num, player_total, map, date, players};
+}
+
+exports.scrape = scrape;
+exports.link = stats_link;
