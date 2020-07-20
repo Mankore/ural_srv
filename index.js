8d231c8a09448a331c1d1aed385a6359836f48c5
Mankor <igorzhelezno@gmail.com>
2020-07-20 12:34:06 +0200

    redone the logic

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { scrape, link } = require('./scrapper');
 const app = express();
 
 const SRV_PORT = 3000;
-const request_every = 4; // minutes
+const request_every = 5; // minutes
 
 app.use(helmet());
 app.use(cors());
@@ -16,60 +16,89 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 let req_date, last_req_date, cached_obj;
+let map_date;
 
-app.use('/info', (req, res, next) => {
-    // If it is a first request after restart
-    // Define last request date
-    if (!last_req_date) {
-        last_req_date = new Date();
-        res.locals.approved = true;
-    } else {
-        // Date of a new request
-        req_date = new Date();
-        // If it is a new request under a ${request_every} minutes
-        // Then send a cached object
-        if (req_date - last_req_date < request_every * 60000) {
-            // console.log('send cached obj');
-            res.locals.approved = false;
-        } else {
-            // console.log('send a new request');
-            // Replace a last_req_date with the latest(new) date
-            last_req_date = req_date;
-            res.locals.approved = true;
-        }
-    }
+// app.use('/info', (req, res, next) => {
+//     // If it is a first request after restart
+//     // Define last request date
+//     if (!last_req_date) {
+//         last_req_date = new Date();
+//         res.locals.approved = true;
+//     } else {
+//         // Date of a new request
+//         req_date = new Date();
+//         // If it is a new request under a ${request_every} minutes
+//         // Then send a cached object
+//         if (req_date - last_req_date < request_every * 60000) {
+//             // console.log('send cached obj');
+//             res.locals.approved = false;
+//         } else {
+//             // console.log('send a new request');
+//             // Replace a last_req_date with the latest(new) date
+//             last_req_date = req_date;
+//             res.locals.approved = true;
+//         }
+//     }
 
-    next();
-})
-
-app.get('/info', (req, res) => {
-    // If Approved, then send a new request
-    // Else send cached object
-    if (res.locals.approved) {
-        scrape(link).then(obj => {
-            cached_obj = obj;
-            res.send(obj);
-        });
-    } else {
-        res.send(cached_obj);
-    }
-});
-
-// app.get('/info', (req, res, next) => {
-//     res.send(cached_obj);
+//     next();
 // })
+
+// app.get('/info', (req, res) => {
+//     // If Approved, then send a new request
+//     // Else send cached object
+//     if (res.locals.approved) {
+//         scrape(link).then(obj => {
+//             cached_obj = obj;
+//             res.send(obj);
+//         });
+//     } else {
+//         res.send(cached_obj);
+//     }
+// });
+
+app.get('/info', (req, res, next) => {
+    res.send(cached_obj);
+});
 
 app.all('*', (req, res) => {
     res.send('Invalid request');
 });
 
 app.listen(process.env.PORT || SRV_PORT, () => {
-    console.log(`App is listening on port: ${process.env.PORT}`);
+    console.log(`App is listening on port: ${process.env.PORT || SRV_PORT}`);
 });
 
-// setInterval(() => {
-//     console.log('Scraping..');
-//     scrape(link).then(obj => {
-//         cached_obj = obj;
-//     }).catch(err => console.log(err));
-// }, request_every * 1000 * 60);
+initialize();
+
+function initialize() {
+    console.log('Initializing requests setInterval');
+    // First start
+    scrape(link).then(obj => {
+        cached_obj = obj;
+        map_date = new Date();
+        cached_obj['map_date'] = map_date;
+        cached_obj['isMapChanged'] = false;
+    }).catch(err => console.log(err));
+    
+    // Scraping every req_every minutes after First Start
+    setInterval(() => {
+        console.log('\nScraping..');
+        scrape(link).then(obj => {
+            console.log(cached_obj.map);
+            console.log(obj.map);
+
+            if (cached_obj['map'] !== obj['map']) {
+                console.log('\x1b[31m%s\x1b[0m', 'MAP CHANGED');
+
+                cached_obj = obj;
+                map_date = new Date();
+                cached_obj['map_date'] = map_date;
+                cached_obj['isMapChanged'] = true;
+            } else {
+                cached_obj = obj;
+                cached_obj['map_date'] = map_date;
+                cached_obj['isMapChanged'] = false;
+            }
+        }).catch(err => console.log(err));
+    }, request_every * 1000 * 60);
+}
