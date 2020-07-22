6833d79f674a88b6aab9174b6649cb6c342222fb
Mankor <igorzhelezno@gmail.com>
2020-07-22 21:56:54 +0200

    cleaned up

--- index.js
+++ index.js
@@ -15,46 +15,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-let req_date, last_req_date, cached_obj;
+let cached_obj;
 let map_date;
-
-// app.use('/info', (req, res, next) => {
-//     // If it is a first request after restart
-//     // Define last request date
-//     if (!last_req_date) {
-//         last_req_date = new Date();
-//         res.locals.approved = true;
-//     } else {
-//         // Date of a new request
-//         req_date = new Date();
-//         // If it is a new request under a ${request_every} minutes
-//         // Then send a cached object
-//         if (req_date - last_req_date < request_every * 60000) {
-//             // console.log('send cached obj');
-//             res.locals.approved = false;
-//         } else {
-//             // console.log('send a new request');
-//             // Replace a last_req_date with the latest(new) date
-//             last_req_date = req_date;
-//             res.locals.approved = true;
-//         }
-//     }
-
-//     next();
-// })
-
-// app.get('/info', (req, res) => {
-//     // If Approved, then send a new request
-//     // Else send cached object
-//     if (res.locals.approved) {
-//         scrape(link).then(obj => {
-//             cached_obj = obj;
-//             res.send(obj);
-//         });
-//     } else {
-//         res.send(cached_obj);
-//     }
-// });
 
 app.get('/info', (req, res, next) => {
     res.send(cached_obj);
@@ -80,7 +42,7 @@ function initialize() {
         cached_obj['isMapChanged'] = false;
     }).catch(err => console.log(err));
     
-    // Scraping every req_every minutes after First Start
+    // Scraping req_every minutes after First Start
     setInterval(() => {
         console.log('\nScraping..');
         scrape(link).then(obj => {
